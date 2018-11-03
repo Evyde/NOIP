e748b81451a74c743f6f4ec255dc9670ebece668
ForeverOpp <life.app.hanfeng@hotmail.com>
2018-11-03 19:25:48 +0800

    Initial Commit

---
+++ 洛谷NOIP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  洛谷NOIP
+//
+//  Created by 韩枫 on 2018/11/3.
+//  Copyright © 2018年 韩枫. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
